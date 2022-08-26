98349ed3e676ff8c04ba4c9591a22c1870d12f45
viettu2k <doanviettu2k@gmail.com>
2022-08-26 13:41:22 +0700

    adding featured post

--- components/Comments.jsx
+++ components/Comments.jsx
@@ -8,9 +8,7 @@ const Comments = ({ slug }) => {
   const [comments, setComments] = useState([]);
 
   useEffect(() => {
-    getComments(slug).then((result) => {
-      setComments(result);
-    });
+    getComments(slug).then((result) => setComments(result));
   }, []);
 
   return (
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,16 @@
+import Head from "next/head";
 import { PostCard, Categories, PostWidget } from "../components";
+import { FeaturedPost } from "../sections";
 import { getPosts } from "../services";
 
 export default function Home({ posts }) {
   return (
     <div className="container mx-auto px-10 mb-8">
+      <Head>
+        <title>Graph CMS Blog</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <FeaturedPost />
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
           {posts.map((post, index) => (
